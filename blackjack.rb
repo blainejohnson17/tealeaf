93ee990fc25319dc93d4f520d8f41039372e1fbe
Blaine Johnson <blainejohnson17@gmail.com>
2013-09-04 01:39:54 -0600

    adds modified total method adds modified total method to account for aces acting as 1 or 11

--- blackjack.rb
+++ blackjack.rb
@@ -14,40 +14,45 @@ def deal(deck, player)
 end
 
 def print player
-  total = 0
-  player.each do |x|
-      puts "#{x[0]}, value: #{x[1]}"
-      total += x[1]
-  end
-  puts "total = #{total}"
+  puts "total: #{total player}"
+  player.each { |x| puts "#{x[0]}, value: #{x[1]}"}
 end
 
 def total player
-  total = 0
-  player.each do |x|
+  change_ace = false
+  begin
+    total = 0
+    aces_high = 0
+    player.each do |x|
+      if x[1] == 11
+        if change_ace
+          x[1] = 1
+          change_ace = false
+        else
+          aces_high += 1
+        end
+      end
       total += x[1]
-  end
+    end
+    change_ace = (total > 21 && aces_high != 0) ? true : false
+  end while change_ace
   total
 end
 
 def dealer_action dealer
-  if (total dealer) < 17
-    '1'
-  else
-    '2'
-  end
+  (total dealer) < 17 ? '1' : '2'
 end
 
-deck = [['ace of hearts', 1], ['two of hearts', 2], ['three of hearts', 3], ['four of hearts', 4], ['five of hearts', 5],
+deck = [['ace of hearts', 11], ['two of hearts', 2], ['three of hearts', 3], ['four of hearts', 4], ['five of hearts', 5],
         ['six of hearts', 6], ['seven of hearts', 7], ['eight of hearts', 8], ['nine of hearts', 9], ['ten of hearts', 10],
         ['jack of hearts', 10], ['queen of hearts', 10], ['king of hearts', 10],
-        ['ace of diamonds', 1], ['two of diamonds', 2], ['three of diamonds', 3], ['four of diamonds', 4], ['five of diamonds', 5],
+        ['ace of diamonds', 11], ['two of diamonds', 2], ['three of diamonds', 3], ['four of diamonds', 4], ['five of diamonds', 5],
         ['six of diamonds', 6], ['seven of diamonds', 7], ['eight of diamonds', 8], ['nine of diamonds', 9], ['ten of diamonds', 10],
-        ['jack of diamonds', 11], ['queen of diamonds', 10], ['king of diamonds', 10],
-        ['ace of clubs', 1], ['two of clubs', 2], ['three of clubs', 3], ['four of clubs', 4], ['five of clubs', 5],
+        ['jack of diamonds', 10], ['queen of diamonds', 10], ['king of diamonds', 10],
+        ['ace of clubs', 11], ['two of clubs', 2], ['three of clubs', 3], ['four of clubs', 4], ['five of clubs', 5],
         ['six of clubs', 6], ['seven of clubs', 7], ['eight of clubs', 8], ['nine of clubs', 9], ['ten of clubs', 10],
         ['jack of clubs', 10], ['queen of clubs', 10], ['king of clubs', 10],
-        ['ace of spades', 1], ['two of spades', 2], ['three of spades', 3], ['four of spades', 4], ['five of spades', 5],
+        ['ace of spades', 11], ['two of spades', 2], ['three of spades', 3], ['four of spades', 4], ['five of spades', 5],
         ['six of spades', 6], ['seven of spades', 7], ['eight of spades', 8], ['nine of spades', 9], ['ten of spades', 10],
         ['jack of spades', 10], ['queen of spades', 10], ['king of spades', 10]]
 
